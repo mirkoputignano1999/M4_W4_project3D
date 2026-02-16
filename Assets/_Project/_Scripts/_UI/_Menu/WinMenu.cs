64daaa6ec6bb001f67bc46c2a6c324adedbe60ad
mirkoputignano1999 <mirko.putignano1999@gmail.com>
2026-02-16 13:48:01 +0100

    win scene

---
+++ Assets/_Project/_Scripts/_UI/_Menu/WinMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "Game";
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
